feb64cd2def4a223add355a98a8329b6914963b0
Francesco Ferreri <fferreri@ixpm.namex.it>
2021-09-28 16:46:20 +0200

    removed requirements

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+requirements = []
 
 setup(
     name="irrhound",
-    version="0.0.1",
+    version="0.1",
     author="Francesco Ferreri",
     author_email="f.ferreri@namex.it",
     description="A package to check for IRR resources",
