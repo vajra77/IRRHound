0dda8413cf3ab7aa2ba5606c1b48922e55c12f1a
Francesco Ferreri <fferreri@ixpm.namex.it>
2021-09-28 16:07:53 +0200

    setup and requirements

--- check_irr_sources.py
+++ check_irr_sources.py
@@ -2,6 +2,11 @@ import sys
 from irrhound import irr_hunt
 
 
+
+if(len(sys.argv) == 1):
+    print("Usage: check_irr_souces <AS number> [<AS set>]")
+    quit()
+
 asn = sys.argv[1]
 if(len(sys.argv) > 2):
     asmacro = sys.argv[2]
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="irrhound",
+    version="0.0.1",
+    author="Francesco Ferreri",
+    author_email="f.ferreri@namex.it",
+    description="A package to check for IRR resources",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vajra77/IRRHound",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: CC0 1.0 Universal",
+    ],
+)
