a08980c0e88d755812f2f1161ef62adae7ea52d5
vajra77 <f.ferreri@namex.it>
2021-09-29 17:17:58 +0200

    Changed code to reflect class refactoring

--- tools/suggest_irr_sources.py
+++ tools/suggest_irr_sources.py
@@ -3,7 +3,7 @@ from irrhound.irrhound import irr_hunt_sources
 
 
 if(len(sys.argv) == 1):
-    print("Usage: suggest_irr_souces <AS number> [<AS set>]")
+    print("Usage: suggest_irr_souces <AS number> [<AS set v4>] [<AS set v6>]")
     quit()
 
 asn = sys.argv[1]
@@ -14,8 +14,13 @@ if 'AS' in asn:
 if(len(sys.argv) > 2):
     asmacro = sys.argv[2]
 else:
-    asmacro = False
+    asmacro = None
 
-suggested = irr_hunt_sources(asn, asmacro)
+if(len(sys.argv) > 3):
+    asmacro6 = sys.argv[3]
+else:
+    asmacro6 = None
+
+suggested = irr_hunt_sources(asn, asmacro, asmacro6)
 
 print("Suggested source list is {}.".format(suggested))
