6272ead7281d61783a8724354b83e7b16ecbccfd
vajra77 <f.ferreri@namex.it>
2024-07-19 14:09:31 +0200

    tool to spot members with resources in RADB

--- tools/ixp_radb_only.py
+++ tools/ixp_radb_only.py
@@ -6,12 +6,15 @@ from irrhound import irrhound
 
 
 MEMBERS = []
+SKIP = ['Arelion', 'China Telecom', 'WindTre', 'Fastweb', 'Colt', 'Hurricane Electric', 'Swisscom']
 
 def load_members(ixf_url):
     response = urlopen(ixf_url)
     data = json.loads(response.read())
     for member in data['member_list']:
         name = member['name']
+        if name in SKIP:
+            continue
         asnum = member['asnum']
         asmacro = None
         for conn in member['connection_list']:
@@ -28,12 +31,13 @@ def load_members(ixf_url):
 if __name__ == "__main__":
     load_members(sys.argv[1])
     with open(sys.argv[2], "w+") as f:
-        for member in MEMBERS:
-            f.write(f"--- {member['name']} ---\n")
+        for m in MEMBERS:
+            f.write(f"--- {m['name']} ---\n")
             try:
-                sources = irrhound.irr_hunt_sources(member['asnum'], member['asmacro'], None)
+                result = irrhound.irr_hunt_sources(m['asnum'], m['asmacro'], None)
+                sources = result['sources']
                 if 'RADB' in sources:
-                    f.write(f"Member {member['name']} has resources in: {sources}\n")
+                    f.write(f"Member {m['name']} has resources in: {sources}\n")
             except Exception as e:
                 f.write(f"ERROR: {e}\n")
             finally:
